89fb1e657935837f4cbd926e0b8968c1a093eb57
VikaasVenkatesh <89876586+VikaasVenkatesh@users.noreply.github.com>
2025-04-11 22:57:39 -0400

    ChatGPT Branch Done and dusted

--- server/chatgpt/router.py
+++ server/chatgpt/router.py
@@ -1,11 +1,41 @@
 from fastapi import APIRouter, status
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+import json
+import re
 
 router = APIRouter()
+dotenv_path = Path(__file__).resolve().parents[2] / ".env"
+loaded = load_dotenv(dotenv_path=dotenv_path)
+api_keyv = os.getenv("OPENAI_API_KEY")
 
 @router.get(
     "/chatgpt",
     status_code=200,
     # response_model=#Create Response Model
 )
+
 def generate_chatgpt_stuff():
-    #do chatgpt stuff
+   client = OpenAI(api_key=api_keyv)
+   mood = "happy"
+   response = client.responses.create(
+      model = "gpt-4o",
+      input = "Give me the name and artist of 15 songs which align with the mood given: " + mood + "Return it in the following JSON schema: [{track: ___, artist: ___}, {track: ____, artist___}, …]. Response should be nothing but the JSON!!"
+    )
+   raw_output = response.output_text
+   cleaned = re.sub(r"```json|```", "", raw_output).strip()
+   try:
+        parsed_json = json.loads(cleaned)
+   except json.JSONDecodeError as e:
+        return {"error": "Failed to parse JSON", "details": str(e), "raw": raw_output}
+   return parsed_json
+
+   
+   
+
+
+
+
+
--- server/main.py
+++ server/main.py
@@ -2,9 +2,30 @@
 # uvicorn main:app --reload
 
 from fastapi import FastAPI
+from chatgpt import router as chatgpt
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex="http://localhost:[0-9]{4,5}",
+    allow_credentials=True,
+    allow_methods=[
+        "GET",
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE",
+    ],
+    allow_headers=["*"]
+
+)
+app.include_router(
+    chatgpt.router,
+    prefix="/chatgpt",
+    tags=["Chatgpt"]
+)
 @app.get("/")
 def read_root():
     return {"message": "Hello suckas!"}
