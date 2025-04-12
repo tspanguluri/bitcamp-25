7bc332eb8ea5f879b5a4b49cd1b7afffb26127c5
Anvay Panguluri <aspanguluri@gmail.com>
2025-04-11 21:19:42 -0400

    added spotify module

--- server/main.py
+++ server/main.py
@@ -1,9 +1,32 @@
 # pip install fastapi "uvicorn[standard]"
-# uvicorn main:app --reload
+# uvicorn main:app --reload OR fastapi dev main.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from spotify import router as spotify
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex="http://localhost:[0-9]{4,5}",
+    allow_credentials=True,
+    allow_methods=[
+        "GET",
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE",
+    ],
+    allow_headers=["*"]
+)
+
+app.include_router(
+    spotify.router,
+    prefix="/spotify",
+    tags="Spotify"
+)
 
 @app.get("/")
 def read_root():
